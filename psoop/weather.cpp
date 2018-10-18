5f3f7d18b10a1bb9cc2418204b0b54be89116e84
Ellzy <31587363+Ellzy@users.noreply.github.com>
2018-10-18 10:58:31 +0530

    unterminated comment asg 1

--- psoop/weather.cpp
+++ psoop/weather.cpp
@@ -1,10 +1,10 @@
 #include<iostream>
 using namespace std;
-class weather_report 
+class weather_report
 { public:
   int day;
   float ht,lt,ra,sa;
-  void accept(); 
+  void accept();
   void display();
   weather_report(){
   day=0;
@@ -15,38 +15,38 @@ class weather_report
   }
   }w[5];
  int i;
- void weather_report::accept() 
+ void weather_report::accept()
  {  cout<<"enter day  of month,"<<"enter high temp,"<<"enter low temp,"<<"enter rain amount,"<<"enter snow      amount,";
-   for(i=0;i<5;i++) 
+   for(i=0;i<5;i++)
 { cin>>w[i].day>>w[i].ht>>w[i].lt>>w[i].ra>>w[i].sa;
  }
  }
  void weather_report::display()
  { cout<<"day of month :"<<"\t"<<"high temp :"<<"\t"<<"low temp :"<<"\t"<<"rain   amount :"<<"snow amount :"<<"\n";
-  for(i=0;i<5;i++){ 
+  for(i=0;i<5;i++){
   cout<<w[i].day<<"\t\t"<<w[i].ht<<"\t\t"<<w[i].lt<<"\t\t"<<w[i].ra<<"\t\t"<<w[i].sa<<"\n";
                                  }
   int s1=0,s2=0,s3=0,s4=0;
    for(i=0;i<5;i++){
            s1=s1+w[i].ht;
-           s2=s2+w[i].lt;                     
+           s2=s2+w[i].lt;
             s3=s3+w[i].ra;
             s4=s4+w[i].sa;
                                   }
                                    cout<<"avg"<<"\t\t"<<s1/5<<"\t\t"<<s2/5<<"\t\t"<<s3/5<<"\t\t"<<s4/5<<"\n";
  }
- int main() 
+ int main()
  { int ch;
   do{ cout<<"enter 1 for accepting data"<<"\n"<<"enter 2 for display data";
    cin>>ch;
    switch(ch){
                case 1:w[5].accept();
                           break;
-               case 2:w[5].display(); 
+               case 2:w[5].display();
                           break;
                default : cout<<"enter 1 or 2";
             }
-  
+
   }while(ch!=3);
 }
 
@@ -92,4 +92,4 @@ day of month :  high temp :     low temp :      rain   amount :snow amount :
 avg             31              24              28              30
 enter 1 for accepting data
 enter 2 for display data
-/*
+*/
